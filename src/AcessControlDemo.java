c787b49388f00aae6d2f89f032f4e1a140e93dc5
anupama.g <anupama.g@L0003-DEV-LBR.AcusisBlr.lan>
2023-02-07 12:13:42 +0530

    First File to commit

---
+++ src/AcessControlDemo.java
@@ -0,0 +1,19 @@
+
+public class AcessControlDemo {
+
+	int a;//default access
+	public int b;//public access
+	private int c;//private access
+	
+	void setC(int i)
+	{
+		c=i;
+	}
+	
+	int getC()
+	{
+		return c;
+	}
+	
+
+}
